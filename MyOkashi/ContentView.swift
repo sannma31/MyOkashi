b7ee969197b31abf1a22d866fb0ab2bd0f6523c7
sannma31 <kasaisyouun2004@gmail.com>
2024-04-29 22:07:51 +0900

    Initial Commit

---
+++ MyOkashi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyOkashi
+//
+//  Created by 笠井翔雲 on 2024/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by 笠井翔雲 on 2024/04/29.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
